5b0110781763679b94846566c2ea1448026f9bd8
F00cked <blacktha@gmail.com>
2019-03-17 15:19:31 +0800

    fix error

--- tcp_bbr.sh
+++ tcp_bbr.sh
@@ -10,8 +10,7 @@ echo -e "${Green_font}
 # Author:   no0ne
 # Blog:     https://sometimesnaive.org
 # Github:   https://github.com/nanqinlang
-#======================================================
-${Font_suffix}"
+#======================================================${Font_suffix}"
 
 check_system(){
 	#sort
